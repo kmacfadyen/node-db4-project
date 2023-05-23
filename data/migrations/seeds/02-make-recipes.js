b206834ba77d1daebcfe0b408691c05c324ddb5f
Kevin MacFadyen <kmacfadyen3@gmail.com>
2023-05-23 17:23:14 -0400

    check four

--- data/migrations/seeds/02-make-recipes.js
+++ data/migrations/seeds/02-make-recipes.js
@@ -1,4 +1,32 @@
+const recipes = [
+    { recipe_name: 'Broccoli Pesto Pasta'},
+    { recipe_name: 'Lemon Chicken'},
+    { recipe_name: 'Salmon en Papillote'},
+]
 
+const ingredients = [
+    { ingredient_name: 'Broccoli', ingredient_unit: 'lbs' },
+    { ingredient_name: 'Pesto', ingredient_unit: 'lbs' },
+    { ingredient_name: 'Pasta', ingredient_unit: 'lbs' },
+    { ingredient_name: 'Lemon', ingredient_unit: 'slices' },
+    { ingredient_name: 'Chicken', ingredient_unit: 'kilos' },
+    { ingredient_name: 'Salmon', ingredient_unit: 'grams' },
+]
+
+const steps = [
+    // Broccoli Pesto Pasta
+    { step_text: '', step_number: 1, recipe_id: 1 },
+    { step_text: '', step_number: 1, recipe_id: 1 },
+    { step_text: '', step_number: 1, recipe_id: 1 },
+    // Lemon Chicken
+    { step_text: '', step_number: 1, recipe_id: 1 },
+    { step_text: '', step_number: 1, recipe_id: 1 },
+    { step_text: '', step_number: 1, recipe_id: 1 },
+    // Salmon en Papillote
+    { step_text: '', step_number: 1, recipe_id: 1 },
+    { step_text: '', step_number: 1, recipe_id: 1 },
+    
+]
 
 exports.seed = function (knex) {
     
